b22100146eb97ed8749281a0a6bcbe0787eab8d1
zhaoziwen <zhaoziwen@zhaoshang.net>
2023-11-05 00:53:00 +0800

    修改host和端口

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,7 +4,7 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1699094331368_5311',
   koa: {
-    port: 7001,
+    port: 8080,
   },
-  host:"192.168.1.40:7001",
+  host:"47.100.242.112:8080",
 } as MidwayConfig;
